ccbcab76967d769745c053b8d7803515e6bcb4b6
huanuan <xieping7006@gmail.com>
2016-09-15 13:47:48 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
+  resources :products
   
   devise_for :users
-    root 'welcome#index'
+    root 'products#index'
 end
