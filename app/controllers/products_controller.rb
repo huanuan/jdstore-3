af1fadeddc4fc73b513434bfc64764aed5c6d7bb
huanuan <xieping7006@gmail.com>
2016-09-19 13:06:38 +0800

    modify image size

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,15 @@ class ProductsController < ApplicationController
 def index
   @products = Product.all
 end
+
 def show
   @product = Product.find(params[:id])
 end
+
+def add_to_cart
+  @product = Product.find(params[:id])
+  redirect_to :back
+end
+
 
 end
