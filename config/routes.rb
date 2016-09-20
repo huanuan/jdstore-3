ea419a242909c228289372b34a312a7b3aedffde
huanuan <xieping7006@gmail.com>
2016-09-20 22:30:10 +0800

    empty cart

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 
   devise_for :users
     root 'products#index'
