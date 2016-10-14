74b9d7f8c7ed2f56a9348c139994e73e6b59e81a
huanuan <xieping7006@gmail.com>
2016-10-14 23:11:17 +0800

    create order state

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,21 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
   end
 
   namespace :account do
     resources :orders
   end
-  
+
   resources :products do
     member do
       post :add_to_cart
@@ -22,10 +31,12 @@ Rails.application.routes.draw do
   end
 
   resources :cart_items
+
   resources :orders do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
   devise_for :users
